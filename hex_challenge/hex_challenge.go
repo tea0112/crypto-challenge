8fa516d352fde7fad0e610338616e4c721e4879d
tea <ducthaidev@gmail.com>
2025-01-08 15:18:35 +0700

    update

--- hex_challenge/hex_challenge.go
+++ hex_challenge/hex_challenge.go
@@ -1,22 +1,25 @@
 package hex_challenge
 
 import (
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 )
 
 func HexChallenge() {
-	HexToBase64()
+	hexStr := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+	fmt.Printf("%s", HexToBase64(hexStr))
 }
 
-func HexToBase64() {
-	trial := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-
-	dst := make([]byte, hex.DecodedLen(len(trial)))
-	n, err := hex.Decode(dst, trial)
+func HexToBase64(hexStr []byte) []byte {
+	dst := make([]byte, hex.DecodedLen(len(hexStr)))
+	n, err := hex.Decode(dst, hexStr)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%s\n", dst[:n])
+	base64Bytes := make([]byte, base64.StdEncoding.EncodedLen(len(dst[:n])))
+	base64.StdEncoding.Encode(base64Bytes, dst)
+
+	return base64Bytes
 }
